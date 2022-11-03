2c5b26ad01c497d0b120c3eca2927d7214606552
Ruben_Van_der_Kelen <ruben.vanderkelen@student.odisee.be>
2022-11-04 00:24:27 +0100

    Buttons w/ JPanel

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,6 +1,4 @@
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
-
 import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -8,12 +6,25 @@ import java.awt.event.*;
 public class TicTacToe implements ActionListener {
 
     JFrame mainFrame;
+    JPanel buttonPanel;
+    JButton[] buttons = new JButton[9];
 
     public void init() {
         mainFrame = new JFrame("Tic Tac Toe");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setSize(700, 500);
 
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(3, 3));
+
+        for(int i = 0; i < 9; i++) {
+            buttons[i] = new JButton();
+            buttonPanel.add(buttons[i]);
+			buttons[i].setFocusable(false);
+			buttons[i].addActionListener(this);
+        }
+        
+        mainFrame.add(buttonPanel);
         mainFrame.setVisible(true);
     }
 
