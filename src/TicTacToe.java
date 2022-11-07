bf28db56249e64be1de7f1c8a0e31aee67fde22d
Ruben_Van_der_Kelen <ruben.vanderkelen@student.odisee.be>
2022-11-07 13:55:15 +0100

    Buttons functionality

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,6 +8,7 @@ public class TicTacToe implements ActionListener {
     JFrame mainFrame;
     JPanel buttonPanel;
     JButton[] buttons = new JButton[9];
+    boolean player1Turn = true;
 
     public void init() {
         mainFrame = new JFrame("Tic Tac Toe");
@@ -20,6 +21,7 @@ public class TicTacToe implements ActionListener {
         for(int i = 0; i < 9; i++) {
             buttons[i] = new JButton();
             buttonPanel.add(buttons[i]);
+            buttons[i].setFont(new Font("Dialog", Font.BOLD, 50));
 			buttons[i].setFocusable(false);
 			buttons[i].addActionListener(this);
         }
@@ -30,7 +32,22 @@ public class TicTacToe implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+        for(int i = 0; i <9;i++) {
+            if(e.getSource() == buttons[i]) {
+                if(player1Turn) {
+                    if(buttons[i].getText().isEmpty()) {
+                        buttons[i].setText("X");
+                        player1Turn = false;
+                    }
+                }
+                else{
+                    if(buttons[i].getText().isEmpty()) {
+                        buttons[i].setText("O");
+                        player1Turn = true;
+                    }                    
+                }
+            }
+        }
         
     }
     
